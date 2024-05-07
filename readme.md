b5a298646f1e08429bab28b700e9365a1a82c746
joshmartinecon <jmartin2013@gmail.com>
2024-05-07 11:20:59 -0400

    updated 5/7/24

--- readme.md
+++ readme.md
@@ -14,7 +14,7 @@ If you generally agree with the above statements, then my web scraping code may 
 
 Below is an example of the output provided by the code for the start of the 2023-24 regular season.
 
-<a><img src="nba_screenshot.png"></a>
+<a><img src="screenshots/nba_screenshot.png"></a>
 
 Future iterations of this may 1) do this for the whole season rather than weekly, 2) display this information in an interactive app form and 3) include the WNBA once their seasons starts back up.
 
@@ -30,13 +30,21 @@ Thus, this code attempts to filter games according to two criteria: quality and 
 
 Below is an example of the output provided by the code for week 9 of the 2023-24 season. The home and overall FPI are omitted from the screenshot for purposes of presentational simplicity.
 
-<a><img src="cfb_screenshot.png"></a>
+<a><img src="screenshots/cfb_screenshot.png"></a>
 
-[Source Code](https://github.com/joshmartinecon/sports-on-tv/blob/main/college%20football.R)
+[Source Code](https://github.com/joshmartinecon/sports-on-tv/blob/main/soccer.R)
 
 ## International Soccer (Fútbol)
 
-Forthcoming.
+Unsurprisingly, many people play the most popular sport in the world. Choosing which games to put on can be a difficult decision. I've tried to make this decision marginally less subjective.
+
+To do so, I provide code for web scraping code to extract data from [Opta Analyst's Football Prediction](https://theanalyst.com/na/2023/08/opta-football-predictions/). This favors men's European football, but they have been gradually expanding to include women's leagues and North American soccer. The advantages of this website is that it provides forecasts, team ratings and kickoff time/date information in one place. The downside is that the data is hidden with javascript table which requires more difficult methods of web scraping.
+
+The current code filters matches based on the 1) overall quality of each of the teams and 2) the relative difference in each club's team quality. Future iterations will add "importance" indicators to measure how much the match will impact the club's chances of promotion/relegation and tournament qualification. Additionally, team ratings are not yet provided for women's teams. These will be incorporated once data quality improves.
+
+<a><img src="screenshots/soccer_screenshot.png"></a>
+
+[Source Code](https://github.com/joshmartinecon/sports-on-tv/blob/main/college%20football.R)
 
 ## National Football League (NFL)
 
@@ -44,9 +52,16 @@ Non-prime time games are still regionally blacked out. Just watch [Redzone](http
 
 ### Loading Packages
 
-Both pieces of code rely upon the rvest and stringr packages in R. You will need to install them if you have not already done so. To do so, run the following commands in the console prior to running the code.
+College Football and NFL webscrapers rely upon the rvest and stringr packages in R. You will need to install them if you have not already done so. To do so, run the following commands in the console prior to running the code.
 
 ```
 install.packages("rvest")
 install.packages("stringr")
+```
+
+The soccer code is significantly more involved and involves the use of RSelenium. This package is fantastic, but is famously senstive. It breaks easily. I recomend installing the [FireFox web browser](https://www.mozilla.org/en-US/firefox/new/) to help minimize issues you may run into using it.
+
+```
+install.packages("RSelenium"")
+install.packages("rvest"")
 ```
---
+++ soccer.R
@@ -0,0 +1,118 @@
+
+library(RSelenium)
+library(rvest)
+
+# Connect to the Selenium Remote Driver
+### Note: You will need Firefox installed
+### Selinium does not play nicely with chrome
+rD <- rsDriver(browser="firefox", port=4444L, verbose=F)
+remDr <- rD[["client"]]
+
+# Go to the webpage
+remDr$navigate("https://theanalyst.com/na/2023/08/opta-football-predictions/")
+
+# Let the page load and read the HTML
+Sys.sleep(5)
+page_source <- remDr$getPageSource()[[1]]
+remDr$switchToFrame(1)
+
+# T ry getting the page source again and look for the table
+page_source <- remDr$getPageSource()[[1]]
+html <- read_html(page_source)
+tables <- html %>% html_nodes("table")
+html %>%
+  html_table() -> z
+
+# Close the page
+remDr$close()
+
+# Clean the data
+y <- list()
+for(i in 1:length(z)){
+  x <- as.data.frame(z[i])
+  if(ncol(x) < 3){
+    next
+  }
+  if("FT" %in% x[,3]){
+    next
+  }
+  if(nrow(x) == 2 & ncol(x) == 4){
+    y[[length(y)+1]] <- data.frame(
+      home = x[1,1],
+      away = x[2,1],
+      home_pr = x[1,2],
+      away_pr = x[2,2],
+      win = as.numeric(gsub("%", "", x[1,3])),
+      lose = as.numeric(gsub("%", "", x[2,3])),
+      draw = as.numeric(gsub("%", "", gsub("DRAW", "", x[1,4])))
+    )
+  }else{
+    next
+  }
+  
+}
+y <- as.data.frame(do.call(rbind, y))
+
+# Extract match dates
+html %>%
+  html_nodes("._match-card-right-label_1u4oy_83") %>%
+  html_text() -> match_dates
+
+# Clean dates & kickoff times
+x <- list()
+for(i in seq(1, length(match_dates), 2)){
+  if("LIVE" == match_dates[i+1] | "" == match_dates[i+1]){
+    next
+  }
+  x[[length(x)+1]] <- data.frame(
+    league = match_dates[i],
+    date = as.data.frame(do.call(rbind, strsplit(match_dates[i+1], " @ ")))$V1,
+    kickoff = as.data.frame(do.call(rbind, strsplit(match_dates[i+1], " @ ")))$V2
+  )
+}
+x <- as.data.frame(do.call(rbind, x))
+z <- as.data.frame(do.call(rbind, strsplit(x$kickoff, ":")))
+z1 <- as.data.frame(do.call(rbind, strsplit(z[,2], " ")))
+x$kickoff <- ifelse(z1$V2 == "PM", paste0(as.numeric(z$V1)+12, ":", z1$V1), 
+                 paste0(as.numeric(z$V1), ":", z1$V1))
+z <- data.frame(
+  abbr = c("Jan", "Feb", "Mar", "Apr", "May", "Jun", 
+           "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"),
+  month = 1:12
+)
+x$date <- as.Date(paste0("2024-", z$month[match(as.data.frame(do.call(rbind, strsplit(x$date, " ")))$V1,
+                                       z$abbr)],
+                         "-", as.data.frame(do.call(rbind, strsplit(x$date, " ")))$V2))
+x$kickoff <- ifelse(as.data.frame(do.call(rbind, strsplit(x$kickoff, ":")))$V1 == 24, 
+                 paste0("12:", as.data.frame(do.call(rbind, strsplit(x$kickoff, ":")))$V2),
+                 x$kickoff)
+x$time <- as.POSIXct(paste(x$date, x$kickoff), 
+                    format = "%Y-%m-%d %H:%M")
+
+# Drop any game that has already happened
+x <- x[x$time >= Sys.time(),]
+'%ni%' <- Negate('%in%')
+x$keep <- 1:nrow(x)
+
+# Drop (for now) WSL matches because they don't have team ratings
+x <- x[x$league %ni% c("WSL"),]
+x <- cbind(x[,1:3], y)
+
+# Drop uneven match ups & low quality teams while always keeping European matches
+x <- x[abs(x$home_pr - x$away_pr) < 10 & 
+         !is.na(x$league) & (x$away_pr + x$home_pr)/2 >= 86 & 
+         x$home_pr >= 80 & x$away_pr >= 80 |
+         x$league == "UCL" | x$league == "UEL",]
+
+# Simplify output
+z <- data.frame(
+  league = x$league,
+  time = as.POSIXct(paste(x$date, x$kickoff), 
+                    format = "%Y-%m-%d %H:%M"),
+  home = x$home,
+  away = x$away,
+  rating = round((x$home_pr + x$away_pr)/2)
+)
+
+# Print output
+z
